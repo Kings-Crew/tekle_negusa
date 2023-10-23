4f10312ee710c5a279618017255fe2a25a80c838
Jimmy kroneld <jimmy.kroneld@hotmail.com>
2023-10-23 19:53:49 +0200

    Implement Server-Sent Events for dynamic counter display
    - Added JavaScript logic to listen for updates from the server using SSE.
    
    - Created a new Flask endpoint (/counter-increment) to stream counter updates.
    
    - Updated counter.html to display real-time counter values using CSS animations.
    
    - Styled the counter for better visibility and aesthetics.

--- Flask_Frontend/app.py
+++ Flask_Frontend/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, stream_with_context
+import time
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('counter.html')
+
+@app.route('/counter-increment')
+def counter_increment():
+    def generate():
+        count = 1
+        while count <= 20:
+            time.sleep(1)
+            yield f"data: {count}\n\n"
+            count += 1
+    return Response(stream_with_context(generate()), content_type='text/event-stream')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
---
+++ Flask_Frontend/static/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var counterTens = new Counter(document.querySelector('[data-js="counter-tens"]')),
+        counterOnes = new Counter(document.querySelector('[data-js="counter-ones"]'));
+
+    function Counter(el) {
+        var current = el.querySelector('[data-js="current"]'),
+            next = el.querySelector('[data-js="next"]'),
+            count,
+            timeout;
+
+        function update(value) {
+            if (count === value) { return; }
+            count = value;
+            next.innerHTML = count;
+            el.classList.add('is-changing');
+            window.clearTimeout(timeout);
+            timeout = window.setTimeout(function() {
+                current.innerHTML = next.innerHTML;
+                el.classList.remove('is-changing');
+            }, 210);
+        }
+
+        return {
+            update: update
+        };
+    }
+
+    // Listen to updates from the server using SSE
+    var evtSource = new EventSource("/counter-increment");
+    evtSource.onmessage = function(event) {
+        var count = parseInt(event.data, 10);  // Convert the received data to an integer
+
+        var tens = Math.floor(count / 10);
+        var ones = count % 10;
+
+        counterTens.update(tens);
+        counterOnes.update(ones);
+    }
+});
