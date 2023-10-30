31459f2aff6fa3dfdee63048f55aa79348d40a40
donsueco <bam.mozafar@gmail.com>
2023-10-30 16:52:39 +0100

    Jag har döpt om database.py till mongodbase.py och lagt in rätt sätt för oss att koppla upp mot vår databas. Jag gjorde detta med hjälp av en .env fil för att öka säkerheten. Jag implementerade detta via load_dotenv biblioteket.

---
+++ main.py
@@ -0,0 +1,4 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("server.appAPI:app", host="0.0.0.0", port=8000, reload=True) #server.appAPI:app motsvarar mappen server, appAPI motsvarar appAPI.py och ":app" motsvarar instansen skapad för FastAPI. #host="0.0.0.0" motsvarar localhost.
