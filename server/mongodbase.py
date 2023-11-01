3e130c68aaa8ab318901285828125f1c493fc93d
Jimmy <53408265+Shallange@users.noreply.github.com>
2023-11-01 17:02:38 +0100

    Revert "Jag har döpt om database.py till mongodbase.py och lagt in rätt sätt för oss att koppla upp mot vår databas. Jag gjorde detta med hjälp av en .env fil för att öka säkerheten. Jag implementerade detta via load_dotenv biblioteket."
    This reverts commit 31459f2aff6fa3dfdee63048f55aa79348d40a40.

--- server/mongodbase.py
+++ server/mongodbase.py
@@ -1,19 +1,2 @@
 #databasanslutningsinformationen där man definierade anslutningsinformationen.
 
-from dotenv import load_dotenv
-import os
-import pymongo
-import dns
-
-# Ladda variabler från .env-filen
-load_dotenv()
-
-# Hämta och läs anslutningssträngen från .env
-connection_string = os.getenv('connectionString')
-
-# Anslut mot MongoDB-klient med den hämtade anslutningssträngen
-if connection_string:
-    client = pymongo.MongoClient(connection_string)
-else:
-    print("No connection string provided.")
-
