a3d0717fa8042c1298758862d16bc38a1db56dae
Marcus Peterson <marcus.peterson@yh.nackademin.se>
2023-11-01 22:19:39 +0100

    LAde till app variabeln

--- Mongodatabase/main.py
+++ Mongodatabase/main.py
@@ -1,4 +1,5 @@
 import uvicorn
-
+from fastapi import FastAPI
+app = FastAPI()
 if __name__ == "__main__":
     uvicorn.run("Mongodatabase.server.appAPI:app", host="0.0.0.0", port=8000, reload=True) #server.appAPI:app motsvarar mappen server, appAPI motsvarar appAPI.py och ":app" motsvarar instansen skapad för FastAPI. #host="0.0.0.0" motsvarar localhost.
