ea6c33d56d3933aa97b83744e70a417420f584a0
Marcus Peterson <marcus.peterson@yh.nackademin.se>
2023-11-01 21:58:34 +0100

    Tog bort Database-cmc mappen

--- Database-CMC/api_database.py
+++
@@ -1,9 +0,0 @@
-#Api byggt med hjälp av FastAPI
-#CMC står för: Control Management Center
-from fastapi import FastAPI
-
-app = FastAPI()
- 
-@app.get("/")
-def root ():
-  return {"message": "Hello World!"}
